052213330c04481f099b705010c6530487361339
misocharles <misocharles@gmail.com>
2015-11-18 23:55:04 +0900

    예제 실습 중
    - http://www.raywenderlich.com/30200/avfoundation-tutorial-adding-overlays-and-animations-to-videos

--- ViewController3.swift
+++ ViewController3.swift
@@ -20,4 +20,40 @@ class ViewController3: UIViewController {
         self.view.layer.addSublayer(playerLayer)
         player.play()
     }
+    
+    //http://www.raywenderlich.com/30200/avfoundation-tutorial-adding-overlays-and-animations-to-videos
+    
+    func testImage() {
+        let image: UIImage = UIImage()
+        image.imageWithColor(UIColor.blueColor(), rectSize: nil)
+    }
+}
+
+extension UIImage {
+
+    func imageWithColor(tintColor: UIColor, rectSize: CGRect?) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        
+        let context = UIGraphicsGetCurrentContext()! as CGContextRef
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, CGBlendMode.Normal)
+
+        let rect: CGRect
+        if rectSize == nil {
+            rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        }
+        else {
+            rect = rectSize!
+        }
+
+        CGContextClipToMask(context, rect, self.CGImage)
+        tintColor.setFill()
+        CGContextFillRect(context, rect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
 }
